569998e91a7c372034935e72085625f6745a58f4
Ethan Dirkes <eldirkes@gmail.com>
2024-09-19 14:17:03 -0500

    Adjusted light source generation / screen resolution

--- globals.gd
+++ globals.gd
@@ -2,7 +2,7 @@ extends Node
 
 var planet_render_res : Vector2
 var screen_size : Vector2
-var screen_render_res_factor : int = 1
+var screen_render_res_factor : int = 2
 
 func _ready() -> void:
 	screen_size = get_viewport().size
